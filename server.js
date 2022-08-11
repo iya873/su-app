05f3914d2c304291fb0da4d634403ca75af837c4
iya873 <siyana873@gmail.com>
2022-08-11 08:30:53 -0400

    my server is up and running

---
+++ server.js
@@ -0,0 +1,13 @@
+const { application } = require('express');
+const express = require('express');
+const app = express()
+
+const port = 8000
+
+app.get('/', (req, res) => {
+    res.send("Welcome to Steven's Universe!")
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
